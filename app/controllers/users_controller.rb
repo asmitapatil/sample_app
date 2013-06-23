e0a705fbfd95d17d500cf823e784020668346940
asmitapatil <apatil@entrib.com>
2013-06-23 15:56:32 +0530

    Finished Delete part

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,43 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
-  
+  before_filter :admin_user,     only: :destroy
   def new
     @user = User.new
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
   
   def show
     @user = User.find(params[:id])
   end
-  
+
   def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      # Handle a successful save.
+    # Handle a successful save.
     else
       render 'new'
     end
   end
-  
+
   def destroy
-    sign_out 
-    redirect root_path
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
@@ -42,15 +49,27 @@ class UsersController < ApplicationController
     end
   end
   
+  def index
+      @users = User.paginate(page: params[:page])
+  end
+
+
   private
-  
+
   def signed_in_user
+    unless signed_in?
+      store_location
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
-    
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
-    end
-  
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+      redirect_to(root_path) unless current_user.admin?
+  end
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,6 +35,8 @@ describe "Authentication" do
       end
 
       it { should have_selector('title', text: user.name) }
+      
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
@@ -52,7 +54,22 @@ describe "Authentication" do
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+      
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
 
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end
+      end
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -64,7 +81,7 @@ describe "Authentication" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end #
-      end #-
+      end #- in the user controller
     end # for non-signed-in users
     
     describe "as wrong user" do
@@ -82,6 +99,12 @@ describe "Authentication" do
         specify { response.should redirect_to(root_path) }
       end
     end
+    
+    def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
     
   end # authorization
   
